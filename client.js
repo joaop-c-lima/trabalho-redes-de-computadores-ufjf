ec41968fb943fce0b7701104b5fb92a7424ca00e
João Lima <joaop.c.lima12@gmail.com>
2023-07-13 19:03:38 -0300

    Cliente quase finalizado

--- client.js
+++ client.js
@@ -1,9 +1,9 @@
-const { TIMEOUT, LOREM_5_PARAGRAPHS, DGRAM, PACKAGE_SIZE, SERVER_PORT, SERVER_HOST, CHANCE_OF_ERROR } = require('./constants');
+const { TIMEOUT, LOREM_5_PARAGRAPHS, LOREM_20_PARAGRAPHS, DGRAM, PACKAGE_SIZE, SERVER_PORT, SERVER_HOST, CHANCE_OF_ERROR, WINDOW_LIMIT } = require('./constants');
 
 const client = DGRAM.createSocket('udp4');
 let currentExpectedAck = 0;
-let subtexts = splitTextIntoSubtexts(LOREM_5_PARAGRAPHS);
-let finalFlag = subtexts.length - 1;
+let subtexts = splitTextIntoSubtexts(LOREM_20_PARAGRAPHS);
+let finalFlag = subtexts.length;
 let window = 1;
 let finalExpectedAck = -1;
 let packages;
@@ -12,19 +12,32 @@ client.on('message', (msg, rinfo) => {
     const ackPacket = JSON.parse(msg);
     if (currentExpectedAck === ackPacket.ack) {
         currentExpectedAck++
-    } else if (currentExpectedAck > ackPacket.ack){
+    } else if (currentExpectedAck > ackPacket.ack) {
         currentExpectedAck = ackPacket.ack + 1;
     }
 });
 
-setInterval(() => {
-    if (finalExpectedAck != -1 && currentExpectedAck === finalExpectedAck) {
-        window++;
-    } else {
-        window = Math.floor(window/2);
-        if(window === 0){
+const intervalId = setInterval(() => {
+    if (currentExpectedAck === finalFlag) {
+        client.close();
+        clearInterval(intervalId);
+        return;
+    }
+    if (finalExpectedAck != -1) {
+        if (currentExpectedAck === finalExpectedAck) {
+            process.stdout.write("Aumento aditivo: Before window: " + window);
             window++;
+            if(window > WINDOW_LIMIT){
+                window = WINDOW_LIMIT;
+            }
+        } else {
+            process.stdout.write("Decrecimento multiplicativo: Before window: " + window);
+            window = Math.floor(window / 2);
+            if (window === 0) {
+                window++;
+            }
         }
+        console.log(" -> After window: " + window + "\n\n");
     }
     finalExpectedAck = currentExpectedAck + window;
     packages = subtexts.slice(currentExpectedAck, finalExpectedAck);
@@ -38,8 +51,10 @@ function sendPackages(packages) {
             client.send(packet, SERVER_PORT, SERVER_HOST, (err) => {
                 if (err) {
                     console.error('Erro ao enviar pacote: ', err);
-                } else console.log("Pacote enviado: ", packet)
+                } else console.log("Pacote enviado: ", JSON.parse(packet).id)
             })
+        } else {
+            console.log("Erro ao enviar pacote: " + JSON.parse(packet).id);
         }
     });
 
--- server.js
+++ server.js
@@ -1,20 +1,24 @@
 const { TIMEOUT, LOREM_5_PARAGRAPHS, DGRAM, PACKAGE_SIZE, SERVER_PORT, SERVER_HOST, CHANCE_OF_ERROR} = require('./constants');
 
 const server = DGRAM.createSocket('udp4');
-let expected
+let expectedAck = 0;
 
 server.on('listening', () => console.log("UDP server listening"));
 server.on('message', (msg, rinfo) => {
-    //console.log(`${rinfo.address}:${rinfo.port} - ${msg}`);
+    if(expectedAck == JSON.parse(msg).id){
+        expectedAck++;
+    }
     let request = JSON.stringify({
-        ack: JSON.parse(msg).id
-    })
+        ack: expectedAck - 1
+    });
     if (Math.random() < CHANCE_OF_ERROR) {
         server.send(request, rinfo.port, rinfo.address, (err) => {
             if (err) {
                 console.error('Erro ao enviar pacote: ', err);
             } else console.log("Pacote enviado: ", request)
         })
+    } else {
+        console.log("Erro ao enviar pacote: " + JSON.parse(request).ack);
     }
 });
 server.bind(SERVER_PORT, SERVER_HOST);
